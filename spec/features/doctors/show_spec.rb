eb265e5b982aa029e36eaae69eae6d82d435179b
Taryn Orlemann <taryn.orlemann@gmail.com>
2022-09-26 09:11:58 -0700

    Test: User Story 2

--- spec/features/doctors/show_spec.rb
+++ spec/features/doctors/show_spec.rb
@@ -17,7 +17,8 @@ RSpec.describe 'Doctor Show Page' do
     doctor_patient_3 = DoctorPatient.create!(doctor_id: doctor_1.id, patient_id: patient_3.id)
     doctor_patient_4 = DoctorPatient.create!(doctor_id: doctor_2.id, patient_id: patient_4.id)
 
-    visit "/doctors/#{doctor_1.id}"
+    #visit "/doctors/#{doctor_1.id}"
+    visit doctor_path(doctor_1)
 
     expect(page).to have_content(doctor_1.name)
     expect(page).to have_content(doctor_1.specialty)
@@ -28,4 +29,24 @@ RSpec.describe 'Doctor Show Page' do
     expect(page).to have_content("Rebecca Pope")
     expect(page).to_not have_content("Zola Shepherd")
   end
+
+  it "can remove patient from doctor's care" do 
+    hospital_1 = Hospital.create!(name: "Grey Sloan Memorial Hospital")
+
+    doctor_1 = Doctor.create!(name: "Meredith Grey", specialty: "General Surgery", university: "Harvard University", hospital_id: hospital_1.id)
+
+    patient_1 = Patient.create!(name: "Katie Bryce", age: 24)
+
+    doctor_patient_1 = DoctorPatient.create!(doctor_id: doctor_1.id, patient_id: patient_1.id)
+
+    visit doctor_path(doctor_1)
+
+    within(".patient-#{patient_1.id}") do
+        expect(page).to have_content("Katie Bryce")
+            expect(page).to have_button("Remove #{patient_1.name}")
+            click_button "Remove #{patient_1.name}"
+            expect(current_path).to eq(doctor_path)
+        end
+    end
+    expect(page).to_not have_content("Katie Bryce")
 end
