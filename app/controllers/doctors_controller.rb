dff5fa1960dd989f046e5e4501734178ce01ccb7
Taryn Orlemann <taryn.orlemann@gmail.com>
2022-09-27 08:26:45 -0700

    Add form to add patient to doctor show page

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,6 +1,7 @@
 class DoctorsController < ApplicationController
     def show
         @doctor = Doctor.find(params[:id])
+        @patients = Patient.all
     end
 
     def destroy
--- spec/features/doctors/show_spec.rb
+++ spec/features/doctors/show_spec.rb
@@ -15,7 +15,6 @@ RSpec.describe 'Doctor Show Page' do
     doctor_patient_1 = DoctorPatient.create!(doctor_id: doctor_1.id, patient_id: patient_1.id)
     doctor_patient_2 = DoctorPatient.create!(doctor_id: doctor_1.id, patient_id: patient_2.id)
     doctor_patient_3 = DoctorPatient.create!(doctor_id: doctor_1.id, patient_id: patient_3.id)
-    doctor_patient_4 = DoctorPatient.create!(doctor_id: doctor_2.id, patient_id: patient_4.id)
 
     #visit "/doctors/#{doctor_1.id}"
     visit doctor_path(doctor_1)
@@ -46,6 +45,30 @@ RSpec.describe 'Doctor Show Page' do
         expect(current_path).to eq(doctor_path(doctor_1))
     end
     expect(page).to_not have_content("Katie Bryce")
-    save_and_open_page
+  end
+
+
+  it "has a form that can add a patient to doctor's care" do 
+    hospital_1 = Hospital.create!(name: "Grey Sloan Memorial Hospital")
+
+    doctor_1 = Doctor.create!(name: "Meredith Grey", specialty: "General Surgery", university: "Harvard University", hospital_id: hospital_1.id)
+
+    patient_1 = Patient.create!(name: "Katie Bryce", age: 24)
+    patient_2 = Patient.create!(name: "Denny Duquette", age: 39)
+    patient_3 = Patient.create!(name: "Rebecca Pope", age: 32)
+    patient_4 = Patient.create!(name: "Zola Shepherd", age: 2)
+
+    doctor_patient_1 = DoctorPatient.create!(doctor_id: doctor_1.id, patient_id: patient_1.id)
+
+    visit doctor_path(doctor_1)
+
+    expect(page).to have_content("Katie Bryce")
+    expect(page).to_not have_content("Denny Duquette")
+
+    fill_in :patient_id, with: "#{patient_1.id}"
+    click_button "Submit"
+
+    expect(current_path).to eq(doctor_path(doctor_1))
+    expect(page).to have_content("Denny Duquette")
   end
 end
